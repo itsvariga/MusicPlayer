6360b18b334cfd802e95d80f1a7ba83d0188f11f
HuangYuGe <845047494@qq.com>
2018-12-27 16:26:44 +0800

    jsonp使用promise改装，并封装一个jsonp请求文件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -32,9 +32,18 @@
     </scroll>
     <router-view></router-view>
   </div> -->
-  <div>
-    推荐页面
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+        </ul>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script type="text/ecmascript-6">
@@ -42,24 +51,25 @@
 // import Loading from 'base/loading/loading'
 // import Scroll from 'base/scroll/scroll'
 // import {getRecommend, getDiscList} from 'api/recommend'
+import {getRecommend} from 'api/recommend'
 // import {playlistMixin} from 'common/js/mixin'
-// import {ERR_OK} from 'api/config'
+import {ERR_OK} from 'api/config'
 // import {mapMutations} from 'vuex'
 
-// export default {
+export default {
 //   mixins: [playlistMixin],
-//   data() {
-//     return {
-//       recommends: [],
-//       discList: []
-//     }
-//   },
-//   created() {
-//     this._getRecommend()
+  data() {
+    return {
+      recommends: []
+      // discList: []
+    }
+  },
+  created() {
+    this._getRecommend()
 
-//     this._getDiscList()
-//   },
-//   methods: {
+    // this._getDiscList()
+  },
+  methods: {
 //     handlePlaylist(playlist) {
 //       const bottom = playlist.length > 0 ? '60px' : ''
 
@@ -78,13 +88,13 @@
 //       })
 //       this.setDisc(item)
 //     },
-//     _getRecommend() {
-//       getRecommend().then((res) => {
-//         if (res.code === ERR_OK) {
-//           this.recommends = res.data.slider
-//         }
-//       })
-//     },
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
 //     _getDiscList() {
 //       getDiscList().then((res) => {
 //         if (res.code === ERR_OK) {
@@ -95,62 +105,62 @@
 //     ...mapMutations({
 //       setDisc: 'SET_DISC'
 //     })
-//   },
+  },
 //   components: {
 //     Slider,
 //     Loading,
 //     Scroll
 //   }
-// }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  // @import "~common/stylus/variable"
+  @import "~common/stylus/variable"
 
-  // .recommend
-  //   position: fixed
-  //   width: 100%
-  //   top: 88px
-  //   bottom: 0
-  //   .recommend-content
-  //     height: 100%
-  //     overflow: hidden
-  //     .slider-wrapper
-  //       position: relative
-  //       width: 100%
-  //       overflow: hidden
-  //     .recommend-list
-  //       .list-title
-  //         height: 65px
-  //         line-height: 65px
-  //         text-align: center
-  //         font-size: $font-size-medium
-  //         color: $color-theme
-  //       .item
-  //         display: flex
-  //         box-sizing: border-box
-  //         align-items: center
-  //         padding: 0 20px 20px 20px
-  //         .icon
-  //           flex: 0 0 60px
-  //           width: 60px
-  //           padding-right: 20px
-  //         .text
-  //           display: flex
-  //           flex-direction: column
-  //           justify-content: center
-  //           flex: 1
-  //           line-height: 20px
-  //           overflow: hidden
-  //           font-size: $font-size-medium
-  //           .name
-  //             margin-bottom: 10px
-  //             color: $color-text
-  //           .desc
-  //             color: $color-text-d
-  //     .loading-container
-  //       position: absolute
-  //       width: 100%
-  //       top: 50%
-  //       transform: translateY(-50%)
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
