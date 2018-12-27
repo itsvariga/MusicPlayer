7541b553d37c62fd08887562367b0c683c54b5a7
HuangYuGe <845047494@qq.com>
2018-12-27 15:28:15 +0800

    页面骨架开发

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// import 'babel-polyfill'
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
-// import fastclick from 'fastclick'
+import router from './router'
+import fastclick from 'fastclick'
 // import VueLazyload from 'vue-lazyload'
 // import store from './store'
 
@@ -11,7 +11,7 @@ import 'common/stylus/index.styl'
 /* eslint-disable no-unused-vars */
 // import vConsole from 'vconsole'
 
-// fastclick.attach(document.body)
+fastclick.attach(document.body)
 
 // Vue.use(VueLazyload, {
 //   loading: require('common/image/default.png')
@@ -20,6 +20,6 @@ import 'common/stylus/index.styl'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render: h => h(App)
 })
