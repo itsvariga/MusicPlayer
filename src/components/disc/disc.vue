3dec8ef03095934bdff4555b97dbd030adc35f8d
HuangYuGe <845047494@qq.com>
2019-01-04 20:32:22 +0800

    歌单详情页面数据抓取/处理/应用

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -6,8 +6,8 @@
 
 <script type="text/ecmascript-6">
 import MusicList from 'components/music-list/music-list'
-// import {getSongList} from 'api/recommend'
-// import {ERR_OK} from 'api/config'
+import {getSongList} from 'api/recommend'
+import {ERR_OK} from 'api/config'
 import {mapGetters} from 'vuex'
 import {createSong} from 'common/js/song'
 
@@ -35,13 +35,13 @@ export default {
     _getSongList() {
       if (!this.disc.dissid) {
         this.$router.push('/recommend')
-        // return
+        return
       }
-      // getSongList(this.disc.dissid).then((res) => {
-      //   if (res.code === ERR_OK) {
-      //     this.songs = this._normalizeSongs(res.cdlist[0].songlist)
-      //   }
-      // })
+      getSongList(this.disc.dissid).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+        }
+      })
     },
     _normalizeSongs(list) {
       let ret = []
