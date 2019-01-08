47e4fac23e2bc78e4a41cfdf2061931975da71fa
HuangYuGe <845047494@qq.com>
2019-01-08 13:32:18 +0800

    查询需要express代理服务（伪装refer）

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -75,6 +75,20 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         }).catch((e) => {
           console.log(e)
         })
+       }),
+       app.get('/api/search', function (req, res) {
+        var url = 'https://c.y.qq.com/qzone-music/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+        axios.get(url, {
+          headers: {
+            referer: 'https://c.y.qq.com/',
+            host: 'c.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          res.json(response.data)
+        }).catch((e) => {
+          console.log(e)
+        })
        })
     }
   },
