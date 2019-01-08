8b7682bea91b3e9cb9ab1a10e30ced8cbcd7250b
HuangYuGe <845047494@qq.com>
2019-01-08 14:45:51 +0800

    no-result基础组件开发

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -17,16 +17,16 @@
       </li>
       <loading v-show="hasMore" title=""></loading>
     </ul>
-    <!-- <div v-show="!hasMore && !result.length" class="no-result-wrapper">
+    <div v-show="!hasMore && !result.length" class="no-result-wrapper">
       <no-result title="抱歉，暂无搜索结果"></no-result>
-    </div> -->
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import Loading from 'base/loading/loading'
-// import NoResult from 'base/no-result/no-result'
+import NoResult from 'base/no-result/no-result'
 import {search} from 'api/search'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
@@ -154,8 +154,8 @@ export default {
   },
   components: {
     Scroll,
-    Loading
-    // NoResult
+    Loading,
+    NoResult
   }
 }
 </script>
