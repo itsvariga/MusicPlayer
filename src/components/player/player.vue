f126ad052199f8075b5642aa98970a6cbe23597a
HuangYuGe <845047494@qq.com>
2019-01-09 16:58:53 +0800

    【播放器】播放功能实现

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -121,7 +121,7 @@
         <div class="middle">
           <div class="middle-l">
             <div class="cd-wrapper" ref="cdWrapper">
-              <div class="cd">
+              <div class="cd" :class="cdCls">
                 <img class="image" :src="currentSong.image">
               </div>
             </div>
@@ -136,7 +136,7 @@
               <i class="icon-prev"></i>
             </div>
             <div class="icon i-center">
-              <i class="icon-play"></i>
+              <i :class="playIcon" @click="togglePlaying"></i>
             </div>
             <div class="icon i-right">
               <i class="icon-next"></i>
@@ -151,19 +151,21 @@
     <transition name="mini">
       <div class="mini-player" v-show="!fullScreen" @click="open">
         <div class="icon">
-          <img width="40" height="40" :src="currentSong.image">
+          <img :class="cdCls" width="40" height="40" :src="currentSong.image">
         </div>
         <div class="text">
           <h2 class="name" v-html="currentSong.name"></h2>
           <p class="desc" v-html="currentSong.singer"></p>
         </div>
         <div class="control">
+          <i @click.stop="togglePlaying" :class="miniIcon"></i>
         </div>
         <div class="control">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <audio ref="audio" :src="currentSong.url"></audio>
   </div>
 </template>
 
@@ -196,15 +198,15 @@ export default {
   //   }
   // },
   computed: {
-    // cdCls() {
-    //   return this.playing ? 'play' : 'play pause'
-    // },
-    // playIcon() {
-    //   return this.playing ? 'icon-pause' : 'icon-play'
-    // },
-    // miniIcon() {
-    //   return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
-    // },
+    cdCls() {
+      return this.playing ? 'play' : 'play pause'
+    },
+    playIcon() {
+      return this.playing ? 'icon-pause' : 'icon-play'
+    },
+    miniIcon() {
+      return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+    },
     // disableCls() {
     //   return this.songReady ? '' : 'disable'
     // },
@@ -214,7 +216,7 @@ export default {
     ...mapGetters([
       // 'currentIndex',
       'fullScreen',
-      // 'playing',
+      'playing',
       'currentSong',
       'playlist'
     ])
@@ -269,15 +271,15 @@ export default {
       this.$refs.cdWrapper.style.transition = ''
       this.$refs.cdWrapper.style[transform] = ''
     },
-    // togglePlaying() {
-    //   if (!this.songReady) {
-    //     return
-    //   }
-    //   this.setPlayingState(!this.playing)
-    //   if (this.currentLyric) {
-    //     this.currentLyric.togglePlay()
-    //   }
-    // },
+    togglePlaying() {
+      // if (!this.songReady) {
+      //   return
+      // }
+      this.setPlayingState(!this.playing)
+      // if (this.currentLyric) {
+      //   this.currentLyric.togglePlay()
+      // }
+    },
     // end() {
     //   if (this.mode === playMode.loop) {
     //     this.loop()
@@ -470,38 +472,42 @@ export default {
       }
     },
     ...mapMutations({
-      setFullScreen: 'SET_FULL_SCREEN'
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayingState: 'SET_PLAYING_STATE'
     })
     // ...mapActions([
     //   'savePlayHistory'
     // ])
-  }
-  // watch: {
-  //   currentSong(newSong, oldSong) {
-  //     if (!newSong.id) {
-  //       return
-  //     }
-  //     if (newSong.id === oldSong.id) {
-  //       return
-  //     }
-  //     if (this.currentLyric) {
-  //       this.currentLyric.stop()
-  //       this.currentTime = 0
-  //       this.playingLyric = ''
-  //       this.currentLineNum = 0
-  //     }
-  //     clearTimeout(this.timer)
-  //     this.timer = setTimeout(() => {
-  //       this.$refs.audio.play()
-  //       this.getLyric()
-  //     }, 1000)
-  //   },
-  //   playing(newPlaying) {
-  //     const audio = this.$refs.audio
-  //     this.$nextTick(() => {
-  //       newPlaying ? audio.play() : audio.pause()
-  //     })
-  //   },
+  },
+  watch: {
+    currentSong(newSong, oldSong) {
+      // if (!newSong.id) {
+      //   return
+      // }
+      // if (newSong.id === oldSong.id) {
+      //   return
+      // }
+      // if (this.currentLyric) {
+      //   this.currentLyric.stop()
+      //   this.currentTime = 0
+      //   this.playingLyric = ''
+      //   this.currentLineNum = 0
+      // }
+      // clearTimeout(this.timer)
+      // this.timer = setTimeout(() => {
+      //   this.$refs.audio.play()
+      //   this.getLyric()
+      // }, 1000)
+      this.$nextTick(() => {
+        this.$refs.audio.play()
+      })
+    },
+    playing(newPlaying) {
+      const audio = this.$refs.audio
+      this.$nextTick(() => {
+        newPlaying ? audio.play() : audio.pause()
+      })
+    }
   //   fullScreen(newVal) {
   //     if (newVal) {
   //       setTimeout(() => {
@@ -509,7 +515,7 @@ export default {
   //       }, 20)
   //     }
   //   }
-  // },
+  }
   // components: {
   //   ProgressBar,
   //   ProgressCircle,
