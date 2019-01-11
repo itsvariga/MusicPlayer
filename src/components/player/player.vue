fa374eb60d0fb3fd887f7af1e52500946aa30409
HuangYuGe <845047494@qq.com>
2019-01-11 14:37:37 +0800

    【播放器】进度条样式、播放时动画

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,105 +1,4 @@
 <template>
-  <!-- <div class="player" v-show="playlist.length>0">
-    <transition name="normal"
-                @enter="enter"
-                @after-enter="afterEnter"
-                @leave="leave"
-                @after-leave="afterLeave"
-    >
-      <div class="normal-player" v-show="fullScreen">
-        <div class="background">
-          <img width="100%" height="100%" :src="currentSong.image">
-        </div>
-        <div class="top">
-          <div class="back" @click="back">
-            <i class="icon-back"></i>
-          </div>
-          <h1 class="title" v-html="currentSong.name"></h1>
-          <h2 class="subtitle" v-html="currentSong.singer"></h2>
-        </div> -->
-        <!-- <div class="middle"
-             @touchstart.prevent="middleTouchStart"
-             @touchmove.prevent="middleTouchMove"
-             @touchend="middleTouchEnd"
-        > -->
-        <!-- <div class="middle">
-          <div class="middle-l" ref="middleL"> -->
-            <!-- <div class="cd-wrapper" ref="cdWrapper">
-              <div class="cd" :class="cdCls">
-                <img class="image" :src="currentSong.image">
-              </div>
-            </div> -->
-            <!-- <div class="playing-lyric-wrapper">
-              <div class="playing-lyric">{{playingLyric}}</div>
-            </div> -->
-          <!-- </div> -->
-          <!-- <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
-            <div class="lyric-wrapper">
-              <div v-if="currentLyric">
-                <p ref="lyricLine"
-                   class="text"
-                   :class="{'current': currentLineNum ===index}"
-                   v-for="(line,index) in currentLyric.lines">{{line.txt}}</p>
-              </div>
-            </div>
-          </scroll> -->
-        <!-- </div> -->
-        <!-- <div class="bottom">
-          <div class="dot-wrapper">
-            <span class="dot" :class="{'active':currentShow==='cd'}"></span>
-            <span class="dot" :class="{'active':currentShow==='lyric'}"></span>
-          </div>
-          <div class="progress-wrapper">
-            <span class="time time-l">{{format(currentTime)}}</span>
-            <div class="progress-bar-wrapper">
-              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
-            </div>
-            <span class="time time-r">{{format(currentSong.duration)}}</span>
-          </div>
-          <div class="operators">
-            <div class="icon i-left" @click="changeMode">
-              <i :class="iconMode"></i>
-            </div>
-            <div class="icon i-left" :class="disableCls">
-              <i @click="prev" class="icon-prev"></i>
-            </div>
-            <div class="icon i-center" :class="disableCls">
-              <i @click="togglePlaying" :class="playIcon"></i>
-            </div>
-            <div class="icon i-right" :class="disableCls">
-              <i @click="next" class="icon-next"></i>
-            </div>
-            <div class="icon i-right">
-              <i @click="toggleFavorite(currentSong)" class="icon" :class="getFavoriteIcon(currentSong)"></i>
-            </div>
-          </div>
-        </div> -->
-      <!-- </div>
-    </transition> -->
-    <!-- <transition name="mini"> -->
-      <!-- <div class="mini-player" v-show="!fullScreen" @click="open"> -->
-        <!-- <div class="icon">
-          <img :class="cdCls" width="40" height="40" :src="currentSong.image">
-          <img width="40" height="40" :src="currentSong.image">
-        </div> -->
-        <!-- <div class="text">
-          <h2 class="name" v-html="currentSong.name"></h2>
-          <p class="desc" v-html="currentSong.singer"></p>
-        </div> -->
-        <!-- <div class="control">
-          <progress-circle :radius="radius" :percent="percent">
-            <i @click.stop="togglePlaying" class="icon-mini" :class="miniIcon"></i>
-          </progress-circle>
-        </div> -->
-        <!-- <div class="control" @click.stop="showPlaylist">
-          <i class="icon-playlist"></i>
-        </div> -->
-      <!-- </div> -->
-    <!-- </transition> -->
-    <!-- <playlist ref="playlist"></playlist>
-    <audio ref="audio" :src="currentSong.url" @play="ready" @error="error" @timeupdate="updateTime"
-           @ended="end"></audio> -->
-  <!-- </div> -->
   <div class="player" v-show="playlist.length > 0">
     <transition name="normal"
                 @enter="enter"
@@ -130,7 +29,9 @@
         <div class="bottom">
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
-            <div class="progress-bar-wrapper"></div>
+            <div class="progress-bar-wrapper">
+              <progress-bar :percent="percent"></progress-bar>
+            </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
           <div class="operators">
@@ -181,7 +82,7 @@
 import {mapGetters, mapMutations} from 'vuex'
 import animations from 'create-keyframe-animation'
 import {prefixStyle} from 'common/js/dom'
-// import ProgressBar from 'base/progress-bar/progress-bar'
+import ProgressBar from 'base/progress-bar/progress-bar'
 // import ProgressCircle from 'base/progress-circle/progress-circle'
 // import {playMode} from 'common/js/config'
 // import Lyric from 'lyric-parser'
@@ -218,9 +119,9 @@ export default {
     disableCls() {
       return this.songReady ? '' : 'disable'
     },
-    // percent() {
-    //   return this.currentTime / this.currentSong.duration
-    // },
+    percent() {
+      return this.currentTime / this.currentSong.duration
+    },
     ...mapGetters([
       'currentIndex',
       'fullScreen',
@@ -524,13 +425,13 @@ export default {
   //       }, 20)
   //     }
   //   }
+  },
+  components: {
+    ProgressBar
+    // ProgressCircle,
+    // Scroll,
+    // Playlist
   }
-  // components: {
-  //   ProgressBar,
-  //   ProgressCircle,
-  //   Scroll,
-  //   Playlist
-  // }
 }
 </script>
 
