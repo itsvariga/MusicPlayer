9b89aed5fbd5862ed8365f075d109221ca757712
HuangYuGe <845047494@qq.com>
2019-01-14 23:01:54 +0800

    歌曲列表组件js逻辑使用mixin抽象

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -116,15 +116,14 @@ import ProgressCircle from 'base/progress-circle/progress-circle'
 import {playMode} from 'common/js/config'
 import Lyric from 'lyric-parser'
 import Scroll from 'base/scroll/scroll'
-// import {playerMixin} from 'common/js/mixin'
+import {playerMixin} from 'common/js/mixin'
 import Playlist from 'components/playlist/playlist'
-import {shuffle} from 'common/js/util'
 
 const transform = prefixStyle('transform')
 const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
-  // mixins: [playerMixin],
+  mixins: [playerMixin],
   data() {
     return {
       songReady: false,
@@ -152,17 +151,10 @@ export default {
     percent() {
       return this.currentTime / this.currentSong.duration
     },
-    iconMode() {
-      return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
-    },
     ...mapGetters([
       'currentIndex',
       'fullScreen',
-      'playing',
-      'currentSong',
-      'playlist',
-      'mode',
-      'sequenceList'
+      'playing'
     ])
   },
   created() {
@@ -306,24 +298,6 @@ export default {
         this.currentLyric.seek(currentTime * 1000)
       }
     },
-    changeMode() {
-      const mode = (this.mode + 1) % 3
-      this.setPlayMode(mode)
-      let list = null
-      if (mode === playMode.random) {
-        list = shuffle(this.sequenceList)
-      } else {
-        list = this.sequenceList
-      }
-      this._resetCurrentIndex(list)
-      this.setPlayList(list)
-    },
-    resetCurrentIndex(list) {
-      let index = list.findIndex((item) => {
-        return item.id === this.currentSong.id
-      })
-      this.setCurrentIndex(index)
-    },
     getLyric() {
       this.currentSong.getLyric().then((lyric) => {
         if (this.currentSong.lyric !== lyric) {
@@ -437,11 +411,7 @@ export default {
       }
     },
     ...mapMutations({
-      setFullScreen: 'SET_FULL_SCREEN',
-      setPlayingState: 'SET_PLAYING_STATE',
-      setCurrentIndex: 'SET_CURRENT_INDEX',
-      setPlayMode: 'SET_PLAY_MODE',
-      setPlayList: 'SET_PLAY_LIST'
+      setFullScreen: 'SET_FULL_SCREEN'
     })
     // ...mapActions([
     //   'savePlayHistory'
--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -4,8 +4,8 @@
       <div class="list-wrapper" @click.stop>
         <div class="list-header">
           <h1 class="title">
-            <i class="icon"></i>
-            <span class="text"></span>
+            <i class="icon" :class="iconMode" @click="changeMode"></i>
+            <span class="text">{{modeText}}</span>
             <span class="clear" @click="showConfirm"><i class="icon-clear"></i></span>
           </h1>
         </div>
@@ -44,15 +44,15 @@
 </template>
 
 <script type="text/ecmascript-6">
-import {mapGetters, mapMutations, mapActions} from 'vuex'
+import {mapActions} from 'vuex'
 import {playMode} from 'common/js/config'
 import Scroll from 'base/scroll/scroll'
 import Confirm from 'base/confirm/confirm'
 // import AddSong from 'components/add-song/add-song'
-// import {playerMixin} from 'common/js/mixin'
+import {playerMixin} from 'common/js/mixin'
 
 export default {
-  // mixins: [playerMixin],
+  mixins: [playerMixin],
   data() {
     return {
       showFlag: false
@@ -60,15 +60,9 @@ export default {
     }
   },
   computed: {
-    // modeText() {
-    //   return this.mode === playMode.sequence ? '顺序播放' : this.mode === playMode.random ? '随机播放' : '单曲循环'
-    // }
-    ...mapGetters([
-      'sequenceList',
-      'currentSong',
-      'playlist',
-      'mode'
-    ])
+    modeText() {
+      return this.mode === playMode.sequence ? '顺序播放' : this.mode === playMode.random ? '随机播放' : '单曲循环'
+    }
   },
   methods: {
     show() {
@@ -103,10 +97,6 @@ export default {
       this.setCurrentIndex(index)
       this.setPlayingState(true)
     },
-    ...mapMutations({
-      setCurrentIndex: 'SET_CURRENT_INDEX',
-      setPlayingState: 'SET_PLAYING_STATE'
-    }),
     scrollToCurrent(current) {
       const index = this.sequenceList.findIndex((song) => {
         return current.id === song.id
