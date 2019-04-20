a18e6570553649ab9796347c31317fbe3f2f9d11
黄宇戈 <845047494@qq.com>
2019-04-21 01:20:10 +0800

    无结果样式更变

--- src/base/no-result/no-result.vue
+++ src/base/no-result/no-result.vue
@@ -26,8 +26,6 @@ export default {
       width: 86px
       height: 90px
       margin: 0 auto
-      bg-image('no-result')
-      background-size: 86px 90px
     .no-result-text
       margin-top: 30px
       font-size: $font-size-medium
